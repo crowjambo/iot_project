73787aec0b506eadfeb79ab6f6a07a6bdbad962a
crowjambo <evaldascrow@gmail.com>
2021-04-20 10:14:20 +0300

    updates

--- serve.js
+++ serve.js
@@ -1,9 +1,21 @@
 var server = require('server');
 var { get } = server.router;
 
+
+// Setup the Raspberry pi
 var rpio = require('rpio');
 
 rpio.open(11, rpio.OUTPUT);
+
+function ledOn() {
+  rpio.open(11, rpio.OUTPUT);
+  rpio.write(11, rpio.HIGH);
+}
+
+function ledOff() {
+  rpio.open(11, rpio.OUTPUT);
+  rpio.write(11, rpio.LOW);
+}
 
 function blink() {
   rpio.write(11, rpio.HIGH);
@@ -12,11 +24,20 @@ function blink() {
   }, 50);
 }
 
-server({ port: 8080 }, [
-  get('/' ,  ctx => {
+server({ port: 3000 }, [
+  get('/', ctx => {
     console.log('a request is coming...');
-    blink();
+    return "default page"
+    //blink();
+  }),
+  get('/off', ctx => {
+    return "LED off"
+    // ledOff()
+  }),
+  get('/on', ctx => {
+    return "Led on"
+    // ledOn()
   }),
 ]);
 
-console.log('server starts on 8080 port');
+console.log('server starts on 3000 port');
